d40a93bbc7b7b51e2fe1ae3f7df32cea1d1351ee
Aman Kumar <thisisaman408@gmail.com>
2024-11-08 04:11:40 +0530

    Added parameters

--- app.js
+++ app.js
@@ -16,7 +16,13 @@ const itemSchema = {
   date: String,
 };
 
+const listSchema = {
+  name: String,
+  items: [itemSchema],
+};
+
 const Item = mongoose.model("Item", itemSchema);
+const List = mongoose.model("List", listSchema);
 const intro = new Item({
   name: "This is a to-do list",
   date: "Friday, 9th November",
@@ -27,7 +33,6 @@ const owner = new Item({
   date: "Friday, 9th November",
 });
 const defaultItems = [intro, owner];
-// let items = [];
 app.get("/", (req, res) => {
   Item.find()
     .then((items) => {
@@ -54,41 +59,94 @@ app.get("/", (req, res) => {
 
 app.get("/:anything", (req, res) => {
   let anything = req.params.anything;
-  console.log(anything);
-});
-
-app.post("/", (req, res) => {
-  var newItem = req.body.newItem;
-  var dated = date.dated(req);
-  let object = new Item({
-    name: newItem,
-    date: dated,
-  });
-  object.save();
-  console.log("Item saved", object._id);
-  res.redirect("/");
-});
-
-app.post("/delete", (req, res) => {
-  const index = new mongoose.Types.ObjectId(String(req.body.index));
-  console.log(index);
-  Item.deleteOne({ _id: index })
-    .then(() => {
-      res.redirect("/");
+  List.findOne({ name: anything })
+    .then((list) => {
+      if (!list) {
+        const list = new List({
+          name: anything,
+          items: defaultItems,
+        });
+        list.save();
+        res.redirect("/" + anything);
+      } else {
+        res.render("list", {
+          kindOfDay: list.name,
+          newList: list.items,
+        });
+      }
     })
     .catch((error) => {
       console.log("Error is : ", error);
     });
 });
 
+app.post("/", (req, res) => {
+  var newItem = req.body.newItem;
+  const title = req.body.title;
+  var dated = date.dated(req);
+  let object = new Item({
+    name: newItem,
+    date: dated,
+  });
+
+  if (title === date.getDate()) {
+    object.save();
+    res.redirect("/");
+  } else {
+    List.findOne({ name: title }).then((list) => {
+      list.items.push(object);
+      list.save();
+      res.redirect("/" + title);
+    });
+  }
+
+  console.log("Item saved", object._id);
+});
+
+app.post("/delete", (req, res) => {
+  const index = new mongoose.Types.ObjectId(String(req.body.index));
+  const listTitle = req.body.title;
+
+  if (listTitle === date.getDate()) {
+    Item.deleteOne({ _id: index })
+      .then(() => {
+        res.redirect("/");
+      })
+      .catch((error) => {
+        console.log("Error is : ", error);
+      });
+  } else {
+    List.findOneAndUpdate(
+      { name: listTitle },
+      { $pull: { items: { _id: index } } }
+    )
+      .then(() => {
+        res.redirect("/" + listTitle);
+      })
+      .catch((error) => {
+        console.log("Error is : ", error);
+      });
+  }
+});
+
 app.post("/update", async (req, res) => {
   try {
     const index = new mongoose.Types.ObjectId(String(req.body.index));
-    console.log(index);
     const newItem = req.body.newItem;
-    await Item.updateOne({ _id: index }, { name: newItem });
-    console.log("Item updated successfully");
-    res.redirect("/");
+    const listTitle = req.body.title;
+
+    if (listTitle === date.getDate()) {
+      await Item.updateOne({ _id: index }, { name: newItem });
+      console.log("Item updated successfully in the main list");
+      res.redirect("/");
+    } else {
+      await List.findOneAndUpdate(
+        { name: listTitle, "items._id": index },
+        { $set: { "items.$.name": newItem } }
+      );
+      console.log("Item updated successfully in the custom list");
+      res.redirect("/" + listTitle);
+    }
   } catch (err) {
     console.error("Error updating item:", err);
     res.status(500).send("Error updating item");
